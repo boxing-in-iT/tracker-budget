7b4021d98b8e5e61cd8394af5fcea576512d6823
boxing-in-It <tbol240@gmail.com>
2024-02-23 15:58:38 +0200

    Add logout function

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LoginPage from "../LoginPage";
+import { fetchData } from "../../helpers/helper";
+import { Navigate, redirect, useLoaderData } from "react-router-dom";
+
+export function dashboardLoader() {
+  const userName = fetchData("userName");
+  return userName;
+}
 
 const Dashboard = () => {
-  let userName = "d"; // Ваше условие должно определять, залогинен пользователь или нет
-  return (
-    <div>
-      {userName ? <div>Hello</div> : <LoginPage isRegistering="Register" />}
-    </div>
-  );
+  const userName = useLoaderData();
+  console.log("UserName: ", userName);
+
+  if (!userName) {
+    // Выполните редирект, обернув его в условие внутри useEffect
+    return <Navigate to={"/register"} />;
+  }
+
+  return userName ? <div>Hello</div> : null;
 };
 
 export default Dashboard;
